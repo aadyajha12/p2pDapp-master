b17f1224deec2d36e457fb854597b867b32f9efc
Aadya Jha <aj534@exeter.ac.uk>
2023-05-08 12:44:05 +0100

    test passing

--- truffle/test/simplestorage.js
+++ truffle/test/simplestorage.js
@@ -1,13 +1,16 @@
 // Import the SimpleStorage contract from the artifacts directory.
 const SimpleStorage = artifacts.require("SimpleStorage");
 
-// Define a new contract test suite that tests the SimpleStorage contract. 
-contract('SimpleStorage', () => {
-  
+// Define a new contract test suite that tests the SimpleStorage contract.
+contract('SimpleStorage', (accounts) => {
+  let simpleStorageInstance;
+
+  beforeEach(async () => {
+    simpleStorageInstance = await SimpleStorage.new();
+  });
+
   // Define a new test case that checks if the newly written values can be read correctly.
   it('should read newly written values', async() => {
-    // Create an instance of the SimpleStorage contract. 
-    const simpleStorageInstance = await SimpleStorage.deployed();
     // Read the initial value from the contract instance, which is expected to be 0.
     var value = (await simpleStorageInstance.read()).toNumber();
 
@@ -29,29 +32,3 @@ contract('SimpleStorage', () => {
     assert.equal(value, 2, "2 was not written");
   });
 });
-
-/*
-Explanation:
-
-This code is a test script for a smart contract named "SimpleStorage". 
-The contract has two functions: "write" and "read" that respectively set and retrieve an integer value. 
-
-The code checks if the contract works as expected by writing values to it and reading them back.
-
-The "const SimpleStorage = artifacts.require("SimpleStorage");" line imports the SimpleStorage contract from the artifacts directory. 
-
-The "contract('SimpleStorage', () => { ... });" block defines a new contract test suite that tests the SimpleStorage contract. 
-
-The "it()" function inside the block defines a new test case that checks if the newly written values can be read correctly.
-
-The "const simpleStorageInstance = await SimpleStorage.deployed();" line creates an instance of the SimpleStorage contract. 
-
-The "var value = (await simpleStorageInstance.read()).toNumber();" line reads the initial value from the contract instance, which is expected to be 0.
-
-The "assert.equal(value, 0, "0 wasn't the initial value");" line verifies that the initial value of the contract is 0. If the value is not 0, it will fail the test case.
-
-The next three blocks of code write values to the contract and read them back to verify if the values are stored correctly. The assert functions verify if the values are correctly stored in the contract. 
-
-Overall, the code tests the SimpleStorage contract to verify that it can store and retrieve values correctly.
-*/
-
